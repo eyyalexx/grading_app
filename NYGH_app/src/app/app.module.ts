32d0daf013f61b1f743d1051fd77ec58ac02211b
eyyalexx <alex.grigorev@hotmail.com>
2018-05-07 13:00:48 -0400

    File upload

---
+++ NYGH_app/src/app/app.module.ts
@@ -0,0 +1,55 @@
+
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB-7o0tKCzuXepyOZK7MxRg2gqyJfJpgmw",
+  authDomain: "nyghdata.firebaseapp.com",
+  databaseURL: "https://nyghdata.firebaseio.com",
+  projectId: "nyghdata",
+  storageBucket: "nyghdata.appspot.com",
+  messagingSenderId: "395157155611"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ NYGH_app/src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  //arrData= Array[];
+  myInput;
+
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+    //this.arrData = db.list('/items').valueChanges();    
+  }
+
+  btnAddClicked(){
+    this.db.list("items/").push(this.myInput);
+  }
+
+}
